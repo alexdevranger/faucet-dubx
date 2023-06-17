fe7f457e8416e4409c057ac438e343743c23ab2f
alexdevranger <aleksandra.vesic.dev@gmail.com>
2023-06-17 10:40:12 +0200

    change logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Testnet Faucet</title>
         <meta name="description" content="Testnet Facuet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/logo.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <Faucet />
